ab331391bc7d6975b57c85ac708b178135f78338
Fernando Martinho <martinho672@gmail.com>
2023-09-07 13:12:04 -0300

    funcao de possibilidade gerada

---
+++ sudokuSolver.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+
+grid = [ [0, 0, 2, 0, 0, 0, 5, 0, 0],
+[0, 1, 0, 7, 0, 5, 0, 2, 0],
+[4, 0, 0, 0, 9, 0, 0, 0, 7],
+[0, 4, 9, 0, 0, 0, 7, 3, 0],
+[8, 0, 1, 0, 3, 0, 4, 0, 9],
+[0, 3, 6, 0, 0, 0, 2, 1, 0],
+[2, 0, 0, 0, 8, 0, 0, 0, 4],
+[0, 8, 0, 9, 0, 2, 0, 6, 0],
+[0, 0, 7, 0, 0, 0, 8, 0, 0],]
+
+
+print(np.matrix(grid))
+
+def possivel(linha, coluna, num):
+    global grid 
+    for i in range(9):
+        if grid[linha][i] == num :
+            return False
+    for i in range(9):
+        if grid[i][coluna] == num :
+            return False
+    colunaAux = (coluna // 3)* 3
+    linhaAux = (linha // 3)* 3
+    for i in range(3):
+        for j in range(3):
+            if grid[linhaAux + i][colunaAux + j] == num :
+                return False  
+    return True
+
+print(possivel(0,0,3))
